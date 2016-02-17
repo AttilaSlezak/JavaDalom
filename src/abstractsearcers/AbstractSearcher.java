bb7eae6c33ff1924bb942df9a34065ee919043a0
Tiba Gabor <tibag7@gmail.com>
2016-02-17 11:54:30 +0100

    AbstractSearch class implemented

---
+++ src/abstractsearcers/AbstractSearcher.java
@@ -0,0 +1,25 @@
+package abstractsearcers;
+
+import java.io.File;
+
+public abstract class AbstractSearcher {
+	
+	protected AbstractSearcher nextSearcher;
+	
+	public void setNextSearcher(AbstractSearcher nextSearcher) {
+		this.nextSearcher = nextSearcher;
+	}
+	
+	public boolean isInResults(File mp3file, String keyword){
+		if(isMatch(mp3file, keyword)){
+			return true;
+		}
+		if(nextSearcher != null){
+			return nextSearcher.isInResults(mp3file, keyword);
+		}else{
+			return false;
+		}
+	}
+	
+	protected abstract boolean isMatch(File mp3file, String keyword);
+}
