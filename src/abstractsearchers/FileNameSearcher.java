0962c0f6df80928441234ccc5c12bdbb5c9947b7
Tiba Gabor <tibag7@gmail.com>
2016-02-17 15:12:02 +0100

    isMatch method in AbstractSearcher class edited

--- src/abstractsearchers/FileNameSearcher.java
+++ src/abstractsearchers/FileNameSearcher.java
@@ -1,17 +1,13 @@
 package abstractsearchers;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class FileNameSearcher extends AbstractSearcher {
 
 	@Override
 	protected boolean isMatch(File mp3file, String keyword) {
 
-		Path p = Paths.get(mp3file.getAbsolutePath());
-		String fileName = p.getFileName().toString();
-		
+		String fileName = mp3file.getName().toString();
 		return fileName.contains(keyword);
 	}
 }
---
+++ src/abstractsearchers/TitleSearcher.java
@@ -0,0 +1,14 @@
+package abstractsearchers;
+
+import java.io.File;
+
+import mp3.ID3Tag;
+
+public class TitleSearcher extends AbstractSearcher {
+
+	@Override
+	protected boolean isMatch(File mp3file, String keyword) {
+		return ID3Tag.id3tagFinder(mp3file).getTitle().contains(keyword);
+	}
+
+}
