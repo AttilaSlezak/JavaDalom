457b0ced387dc97f7a79cb3e23b0c79b20833f22
Slezak Attila <attila.slezak@gmail.com>
2016-02-18 17:46:56 +0100

    GenreSearcher.java changed

--- src/abstractsearchers/GenreSearcher.java
+++ src/abstractsearchers/GenreSearcher.java
@@ -7,9 +7,9 @@ import mp3.ID3Tag;
 public class GenreSearcher extends AbstractSearcher {
 
 	@Override
-	public boolean isMatch(File mp3file, String keyword) {
+	protected boolean isMatch(File mp3file, String keyword) {
 		Integer genreNum = ID3Tag.id3tagFinder(mp3file).getGenre();
-		if (keyword == Integer.toString(genreNum)) {
+		if (keyword.equals(Integer.toString(genreNum))) {
 			return true;
 		}
 		return genreMap.get(genreNum).toLowerCase().contains(keyword);
