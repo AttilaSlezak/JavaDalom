821f2628f3db7c4410ed2827fe9effa79be354e1
Slezak Attila <attila.slezak@gmail.com>
2016-02-18 15:54:45 +0100

    GenreTypes.java added GenreSearcher.java modified

--- src/abstractsearchers/GenreSearcher.java
+++ src/abstractsearchers/GenreSearcher.java
@@ -1,21 +1,18 @@
 package abstractsearchers;
 
 import java.io.File;
-import java.util.Map;
-
-import mp3.GenreTypes;
+import static mp3.GenreTypes.genreMap;
 import mp3.ID3Tag;
 
 public class GenreSearcher extends AbstractSearcher {
 
 	@Override
 	public boolean isMatch(File mp3file, String keyword) {
-		Map<Integer, String> genreTypes = GenreTypes.getGenreTypes();
 		Integer genreNum = ID3Tag.id3tagFinder(mp3file).getGenre();
 		if (keyword == Integer.toString(genreNum)) {
 			return true;
 		}
-		return genreTypes.get(genreNum).contains(keyword);
+		return genreMap.get(genreNum).toLowerCase().contains(keyword);
 	}
 
 }
