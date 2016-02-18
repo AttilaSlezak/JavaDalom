682ac9a590f2d59a627d3d1ae6b7950f6a4a329f
Slezak Attila <attila.slezak@gmail.com>
2016-02-18 16:34:39 +0100

    GenreSearcher added again

---
+++ src/abstractsearchers/GenreSearcher.java
@@ -0,0 +1,18 @@
+package abstractsearchers;
+
+import java.io.File;
+import static mp3.GenreTypes.genreMap;
+import mp3.ID3Tag;
+
+public class GenreSearcher extends AbstractSearcher {
+
+	@Override
+	public boolean isMatch(File mp3file, String keyword) {
+		Integer genreNum = ID3Tag.id3tagFinder(mp3file).getGenre();
+		if (keyword == Integer.toString(genreNum)) {
+			return true;
+		}
+		return genreMap.get(genreNum).toLowerCase().contains(keyword);
+	}
+
+}
