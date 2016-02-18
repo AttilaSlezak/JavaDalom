b2db6ca582cbf9341c9932b131247c4bcd1e6adf
Zsori Lukacs Peter <zsori.lukacs@gmail.com>
2016-02-18 14:23:21 +0100

    Menu class edited, MusicHandler added

--- src/searcher/Menu.java
+++ src/searcher/Menu.java
@@ -3,53 +3,66 @@ package searcher;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import abstractsearchers.AbstractSearcher;
+import abstractsearchers.AlbumSearcher;
+import abstractsearchers.ArtistSearcher;
+import abstractsearchers.CommentSearcher;
+import abstractsearchers.FileNameSearcher;
+import abstractsearchers.GenreSearcher;
+import abstractsearchers.TitleSearcher;
+import abstractsearchers.YearSearcher;
 public class Menu {
     
-    public static List<String> inputForConsol() {
+    public static List<AbstractSearcher> inputForConsol() {
         
        String input = null;
        String keyword = null;
        final String yesAnswer = "y";
        final String noAnswer = "n";
-       List<String> listToSearch = new ArrayList<String>();
+       List<AbstractSearcher> listToSearch = new ArrayList<AbstractSearcher>();
        
-       listToSearch.add("file name");
-       listToSearch.add("title");
-       listToSearch.add("artist");
-       listToSearch.add("album");
-       listToSearch.add("year");
-       listToSearch.add("comment");
-       listToSearch.add("genre");
+       listToSearch.add(new FileNameSearcher());
+       listToSearch.add(new TitleSearcher());
+       listToSearch.add(new ArtistSearcher());
+       listToSearch.add(new AlbumSearcher());
+       listToSearch.add(new YearSearcher());
+       listToSearch.add(new CommentSearcher());
+       listToSearch.add(new GenreSearcher());
+       
+       List<String> menuElements = new ArrayList<String>();
+       
+       menuElements.add("file name");
+       menuElements.add("title");
+       menuElements.add("artist");
+       menuElements.add("album");
+       menuElements.add("year");
+       menuElements.add("comment");
+       menuElements.add("genre");
        
        Scanner inputReader = new Scanner(System.in);
+       do {
+    	   System.out.println("Please write here the keyword for searching: ");
+           keyword = inputReader.nextLine();
+       } while (keyword.equals(""));
        
-       System.out.println("Please write here the keyword for searching: ");
-       keyword = inputReader.nextLine();
 
        for (int i = 0; i < listToSearch.size(); i++) {
             do{
-                System.out.println("Would you like to search in " + listToSearch.get(i) + "? (Y / N): " );
+                System.out.println("Would you like to search in " + menuElements.get(i) + "? (Y / N): " );
                 input = inputReader.nextLine().toLowerCase();
                 if(input.equals(noAnswer)){
                     listToSearch.remove(i);
                     i--;
                 }
                 else if(input.equals(yesAnswer)){
-                     System.out.println(listToSearch.get(i) + " added to search!");
+                     System.out.println(menuElements.get(i) + " added to search!");
                 }else{
                     System.out.println("Wrong answer. Please press Y to 'yes'or N to 'no'!");               
                 }
             }while(!( input.equals(yesAnswer) ||  input.equals(noAnswer)));
        }
-
-       
        inputReader.close();
-        return listToSearch;
+       return listToSearch;
    }
-    
-    public static void main(String[] args) {
-        List<String> result = new ArrayList<String>();
-        result = inputForConsol();
-        System.out.println(result);
-    }
 }
