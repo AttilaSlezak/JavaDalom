9f06f9704a595456cdfb7c31c20144cc059ed005
Kozma Balazs <kozma.balazs1992@gmail.com>
2016-02-18 10:50:56 +0100

    Comment searcher done

---
+++ src/abstractsearchers/CommentSearcher.java
@@ -0,0 +1,15 @@
+package abstractsearchers;
+
+import java.io.File;
+
+import mp3.ID3Tag;
+
+public class CommentSearcher extends AbstractSearcher {
+
+	@Override
+	protected boolean isMatch(File mp3file, String keyword) {
+		
+		return ID3Tag.id3tagFinder(mp3file).getComment().contains(keyword);
+	}
+
+}
